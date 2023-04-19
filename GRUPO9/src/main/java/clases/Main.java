a74c387dd5fe88fe649f0944326f5e45e94d9046
Emanuel Sotelo <emanicosotelo@gmail.com>
2023-04-19 15:35:15 -0300

    asignacion de puntos extra antes de monstrar los resutados parciales por pantalla

--- GRUPO9/src/main/java/clases/Main.java
+++ GRUPO9/src/main/java/clases/Main.java
@@ -13,46 +13,42 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
 
-		try{
-			Class.forName("com.mysql.cj.jdbc.Driver");  
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/grupo9", "root", "root");
 
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/grupo9","root","12345");  
+			Statement stmt = con.createStatement();
 
-			Statement stmt=con.createStatement();  	
-
-			//USO DE LA DB  
-			ResultSet rs=stmt.executeQuery("SELECT num_jugador , participante , equipo1, equipo2, resultado FROM grupo9.pronosticos;"); 
+			// USO DE LA DB
+			ResultSet rs = stmt.executeQuery(
+					"SELECT num_jugador , participante , equipo1, equipo2, resultado FROM grupo9.pronosticos;");
 
 			String Resultados = "src\\main\\java\\Archivos\\Resultados.txt";
-			//String Pronosticos = "src\\main\\java\\Archivos\\Pronostico.txt";
 			String espacio = "********************************************************************";
 			String espacioRonda = "--------------------------------------------------------------------";
-			
-			
+
 			Scanner scn = new Scanner(System.in);
 			System.out.println("Ingrese el valor de puntos a otorgar por cada acierto:");
-			int numero  = 1;
-			while(true){
+			int numero = 1;
+			while (true) {
 				try {
-	                numero = scn.nextInt();
-	                break;
-	            } catch (InputMismatchException e) {
-	                System.out.print("Por favor ingresa un numero valido: ");
-	                scn.next();
-	            }
+					numero = scn.nextInt();
+					break;
+				} catch (InputMismatchException e) {
+					System.out.print("Por favor ingresa un numero valido: ");
+					scn.next();
+				}
 			}
 			scn.close();
 			List<Ronda> rondas = rondas(Resultados);
-			List<Participante> participantes = participantes(rs,numero);
+			List<Participante> participantes = participantes(rs, numero);
 			partida(rondas, espacio, espacioRonda, participantes);
-			
-			con.close(); 
-		}
-		catch(Exception e){ 
-			System.out.println(e);
-		}  
 
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
 	}
 
@@ -62,6 +58,10 @@ public class Main {
 		int nroPartido = -1;
 		for (int i = 0; i < rondas.size(); i++) {
 			System.out.println("\t\tRonda numero: " + rondas.get(i).getNumRonda());
+			for (int p = 0; p < participantes.size(); p++) {// En caso de que se hayan equivocado volvemos a setear el
+															// valor de la bandera
+				participantes.get(p).setRonda_perfecta(true);
+			}
 			for (int j = 0; j < rondas.get(i).getPartidos().size(); j++) {
 				nroPartido++;
 				System.out.println("Datos del partido " + (j + 1) + rondas.get(i).getPartidos().get(j));
@@ -75,17 +75,29 @@ public class Main {
 					if (participantes.get(k).getPronosticos().get(nroPartido).getRersultado()
 							.equalsIgnoreCase(rondas.get(i).getPartidos().get(j).equipoGanador())) {
 						participantes.get(k).acierto();
-					}
-					else {
+					} else {
 						participantes.get(k).setPartida_perfecta(false);
 						participantes.get(k).setRonda_perfecta(false);
 					}
 					System.out.println(espacioRonda);
 				}
-				
+
+				if (j == ((rondas.get(i).getPartidos().size()) - 1)) {// Si es el ultimo partido de la ronda evalua si
+																		// hubo
+																		// errores en la ronda y asigna puntos extra.
+					for (int l = 0; l < participantes.size(); l++) {
+						if (participantes.get(l).isRonda_perfecta()) {
+							participantes.get(l).ronda_perfecta();
+						}
+					}
+				}
 				System.out.println("Resultados parciales:");
 				for (int k = 0; k < participantes.size(); k++) {
 					System.out.println(participantes.get(k).mostrarResultados());
+					if (participantes.get(k).isRonda_perfecta() & j == rondas.get(i).getPartidos().size() - 1) {
+						//si es el ultimo partido de la ronda y la la ronda es perfecta felicita al jugador.
+						System.out.println("\t\tRonda perfecta!");
+					}
 					System.out.println(espacioRonda);
 
 				}
@@ -93,26 +105,20 @@ public class Main {
 
 			}
 			System.out.println("\n\n");
-			for (int l = 0; l < participantes.size(); l++) {
-				if(participantes.get(l).isRonda_perfecta()) {
-					participantes.get(l).ronda_perfecta();
-				}
-			}
+
 		}
-		
+
 		System.out.println("Resultados Finales:");
 		for (int i = 0; i < participantes.size(); i++) {
-			if(participantes.get(i).isPartida_perfecta()) {
+			if (participantes.get(i).isPartida_perfecta()) {
 				participantes.get(i).partida_perfecta();
-				System.out.println(participantes.get(i).mostrarResultados()+"   Partida Perfecta!!!\n");
-			}
-			else {
-			System.out.println(participantes.get(i).mostrarResultados()+"\n");
+				System.out.println(participantes.get(i).mostrarResultados() + "   Partida Perfecta!!!\n");
+			} else {
+				System.out.println(participantes.get(i).mostrarResultados() + "\n");
 			}
 		}
 	}
 
-	
 	private static List<Ronda> rondas(String Resultados) {
 		List<Ronda> rondas = new ArrayList<Ronda>();
 		int indiceRonda = -1;
@@ -125,8 +131,7 @@ public class Main {
 					List<Partido> partidos = new ArrayList<Partido>();
 					rondas.add(new Ronda(numRonda, partidos));
 					indiceRonda++;
-				}
-				else if (!linea.isEmpty()) {
+				} else if (!linea.isEmpty()) {
 					String equipo1 = linea.split(",")[0];
 					int goles1 = Integer.parseInt(linea.split(",")[1]);
 					String equipo2 = linea.split(",")[3];
@@ -143,61 +148,51 @@ public class Main {
 		return rondas;
 	}
 
-	private static List<Participante> participantes(ResultSet rs,int numero) {
+	private static List<Participante> participantes(ResultSet rs, int numero) {
 		List<Participante> participantes = new ArrayList<Participante>();
 		String nombre = "";
 		try {
-			while(rs.next()) {
+			while (rs.next()) {
 
-				if(!nombre.equalsIgnoreCase(rs.getString(2))) {
+				if (!nombre.equalsIgnoreCase(rs.getString(2))) {
 					List<Pronostico> pronosticos = new ArrayList<Pronostico>();
 					participantes.add(new Participante(rs.getString(2), pronosticos));
-					participantes.get(Integer.parseInt(rs.getString(1))-1).setValor_puntos(numero);;
+					participantes.get(Integer.parseInt(rs.getString(1)) - 1).setValor_puntos(numero);
+					;
 					nombre = rs.getString(2);
 				}
 				String equipo1 = rs.getString(3);
 				String equipo2 = rs.getString(4);
 				String resultado = rs.getString(5);
 				Pronostico pronostico = new Pronostico(equipo1, equipo2, resultado);
-				participantes.get(Integer.parseInt(rs.getString(1))-1).agregarPronostico(pronostico);
+				participantes.get(Integer.parseInt(rs.getString(1)) - 1).agregarPronostico(pronostico);
 			}
 
-
-		}
-		catch(Exception e){ 
+		} catch (Exception e) {
 			System.out.println(e);
-		}  
+		}
 		return participantes;
 	}
 	/*
-	private static List<Participante> participantes(String Pronosticos) {
-		List<Participante> participantes = new ArrayList<Participante>();
-		int indiceParticipante = -1;
-		String nombre = "";
-
-		try {
-			for (String linea : Files.readAllLines(Paths.get(Pronosticos))) {
-				if (linea.split(",")[0].equalsIgnoreCase("Participante")) {
-					nombre = linea.split(",")[1];
-					List<Pronostico> pronosticos1 = new ArrayList<Pronostico>();
-					participantes.add(new Participante(nombre, pronosticos1));
-					indiceParticipante++;
-
-				}
-				else if (!linea.isEmpty()) {
-					String equipo1 = linea.split(",")[0];
-					String equipo2 = linea.split(",")[1];
-					String resultado = linea.split(",")[2];
-					Pronostico pronostico = new Pronostico(equipo1, equipo2, resultado);
-					participantes.get(indiceParticipante).agregarPronostico(pronostico);
-
-				}
-
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return participantes;
-	}*/
+	 * private static List<Participante> participantes(String Pronosticos) {
+	 * List<Participante> participantes = new ArrayList<Participante>(); int
+	 * indiceParticipante = -1; String nombre = "";
+	 * 
+	 * try { for (String linea : Files.readAllLines(Paths.get(Pronosticos))) { if
+	 * (linea.split(",")[0].equalsIgnoreCase("Participante")) { nombre =
+	 * linea.split(",")[1]; List<Pronostico> pronosticos1 = new
+	 * ArrayList<Pronostico>(); participantes.add(new Participante(nombre,
+	 * pronosticos1)); indiceParticipante++;
+	 * 
+	 * } else if (!linea.isEmpty()) { String equipo1 = linea.split(",")[0]; String
+	 * equipo2 = linea.split(",")[1]; String resultado = linea.split(",")[2];
+	 * Pronostico pronostico = new Pronostico(equipo1, equipo2, resultado);
+	 * participantes.get(indiceParticipante).agregarPronostico(pronostico);
+	 * 
+	 * }
+	 * 
+	 * }
+	 * 
+	 * } catch (IOException e) { e.printStackTrace(); } return participantes; }
+	 */
 }
--- GRUPO9/src/main/java/clases/Partido.java
+++ GRUPO9/src/main/java/clases/Partido.java
@@ -1,15 +1,15 @@
 package clases;
+
 public class Partido {
 	private String equipo1;
 	private String equipo2;
 	private int cantidadGolesE1;
 	private int cantidadGolesE2;
 
-
-
 	public Partido() {
 		super();
 	}
+		
 	public Partido(String equipo1, String equipo2, int cantidadGolesE1, int cantidadGolesE2) {
 		super();
 		this.equipo1 = equipo1;
@@ -17,49 +17,57 @@ public class Partido {
 		this.cantidadGolesE1 = cantidadGolesE1;
 		this.cantidadGolesE2 = cantidadGolesE2;
 	}
+
 	public String getEquipo1() {
 		return equipo1;
 	}
+
 	public void setEquipo1(String equipo1) {
 		this.equipo1 = equipo1;
 	}
+
 	public String getEquipo2() {
 		return equipo2;
 	}
+
 	public void setEquipo2(String equipo2) {
 		this.equipo2 = equipo2;
 	}
+
 	public int getCantidadGolesE1() {
 		return cantidadGolesE1;
 	}
+
 	public void setCantidadGolesE1(int cantidadGolesE1) {
 		this.cantidadGolesE1 = cantidadGolesE1;
 	}
+
 	public int getCantidadGolesE2() {
 		return cantidadGolesE2;
 	}
+
 	public void setCantidadGolesE2(int cantidadGolesE2) {
 		this.cantidadGolesE2 = cantidadGolesE2;
 	}
 
 	public String equipoGanador() {
-		String ganador; 
-		if (this.cantidadGolesE1>this.cantidadGolesE2) {
-			ganador=this.equipo1;
-		}else {
-			if (this.cantidadGolesE2>this.cantidadGolesE1) {
-				ganador=this.equipo2;
-			}else {
-				ganador="Empate";
+		String ganador;
+		if (this.cantidadGolesE1 > this.cantidadGolesE2) {
+			ganador = this.equipo1;
+		} else {
+			if (this.cantidadGolesE2 > this.cantidadGolesE1) {
+				ganador = this.equipo2;
+			} else {
+				ganador = "Empate";
 			}
 		}
 		return ganador;
 	}
+
 	@Override
 	public String toString() {
 		return "\nEquipo 1: " + equipo1 + "\nCantidad de goles: " + cantidadGolesE1 + "\n\nEquipo 2: " + equipo2
-				+ "\nCantidad de goles: " + cantidadGolesE2+"\n";
+				+ "\nCantidad de goles: " + cantidadGolesE2 + "\n";
 	}
-	
-	
+
 }
